cfabf3bf5d2a5360bbc10c0abae1dc465dfaa37f
Denis Shevchenko <den.sheva2000@gmail.com>
2026-01-25 17:30:24 +0200

    notification channel creation

--- app/src/main/java/com/example/androidcourseshpp/ui/App.kt
+++ app/src/main/java/com/example/androidcourseshpp/ui/App.kt
@@ -1,7 +1,38 @@
 package com.example.androidcourseshpp.ui
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+import com.example.androidcourseshpp.R
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : Application()
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                NOTIFICATION_CHANNEL_ID,
+                NOTIFICATION_CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_LOW
+            ).apply {
+                description = getString(R.string.channel_description)
+            }
+
+            val notificationManager =
+                getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    companion object {
+        const val NOTIFICATION_CHANNEL_ID = "contactsManagementChannel"
+        const val NOTIFICATION_CHANNEL_NAME = "Contacts"
+    }
+}
