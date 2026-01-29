b0e8a4225deea4ee5dbcafc28b68f7e5f2e7ac97
Denis Shevchenko <den.sheva2000@gmail.com>
2026-01-29 12:00:55 +0200

    delete unused import

--- app/src/main/java/com/example/androidcourseshpp/domain/usecase/contacts/AddContactUseCase.kt
+++ app/src/main/java/com/example/androidcourseshpp/domain/usecase/contacts/AddContactUseCase.kt
@@ -1,6 +1,5 @@
 package com.example.androidcourseshpp.domain.usecase.contacts
 
-import com.example.androidcourseshpp.domain.entity.contact.ContactInfo
 import com.example.androidcourseshpp.domain.repository.ContactsRepository
 import javax.inject.Inject
 import javax.inject.Singleton
